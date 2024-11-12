fd2de9f9ed616ffab0d0c0f147f24b9da4278312
David Singleton <david.singleton@ucd.ie>
2024-11-12 15:20:39 +0000

    Removing this algorithm as not validated in the TVS

--- TD_ElGohary/Library/add_metadata.m
+++
@@ -1,5 +0,0 @@
-function add_metadata(dirname, filename, textRow)
-    fid = fopen(fullfile(dirname, filename), 'a');
-    fprintf(fid, strcat(textRow, '\n'));
-    fclose(fid);
-end
--- TD_ElGohary/gsd_dummy/Library/standards_present.m
+++
@@ -1,8 +0,0 @@
-function valid = standards_present(data)
-if isfield(data, 'Standards')
-    valid = 1;
-else
-    valid = 0;
-end
-end
-
